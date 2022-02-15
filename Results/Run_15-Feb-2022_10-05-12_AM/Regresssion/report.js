c544d501b6e4a6ebc291c0258fa57b70ba66837d
16191A0506 <mungarakirandeep@gmail.com>
2022-02-15 10:06:06 +0530

    test

---
+++ Results/Run_15-Feb-2022_10-05-12_AM/Regresssion/report.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("DigitalShopify.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Digital Shopify",
+  "description": "As a user, I want to run regression tests in Digital Shopify application",
+  "id": "digital-shopify",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 6543038100,
+  "status": "passed"
+});
+formatter.background({
+  "line": 4,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 5,
+  "name": "I am on homepage",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "DigitalShopifyStepDefs.i_am_on_homepage()"
+});
+formatter.result({
+  "duration": 8665588800,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 8,
+  "name": "Login in Digital Shopify Application",
+  "description": "",
+  "id": "digital-shopify;login-in-digital-shopify-application",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 7,
+      "name": "@digital"
+    }
+  ]
+});
+formatter.step({
+  "line": 9,
+  "name": "I click on login",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "I should be logged in",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "DigitalShopifyStepDefs.i_click_on_login()"
+});
+formatter.result({
+  "duration": 3600356800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "DigitalShopifyStepDefs.i_should_be_logged_in()"
+});
+formatter.result({
+  "duration": 1715959400,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 133200,
+  "status": "passed"
+});
+});
